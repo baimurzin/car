ec024a978ac0e3208b3509b4a7f8ae94a679975c
Zahit Talipov <zahittalipov@gmail.com>
2016-01-18 15:30:56 +0300

    added route complete

---
+++ app/src/main/java/application/com/car/fragments/TimeChoiceFragment.java
@@ -0,0 +1,7 @@
+package application.com.car.fragments;
+
+/**
+ * Created by Zahit Talipov on 18.01.2016.
+ */
+public class TimeChoiceFragment {
+}
---
+++ app/src/main/java/application/com/car/listeners/ChoiceDatetimeListener.java
@@ -0,0 +1,7 @@
+package application.com.car.listeners;
+
+/**
+ * Created by Zahit Talipov on 18.01.2016.
+ */
+public class ChoiceDatetimeListener {
+}
